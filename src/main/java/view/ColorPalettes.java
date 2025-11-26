ebade4a837f2e3df9ec9b709d9cef68f7657f556
KaixinHuang-Angel <kaixinangel.huang@mail.utoronto.ca>
2025-11-26 17:26:45 -0500

    changed some of the colors you can choose from for add event in calendar

--- src/main/java/view/ColorPalettes.java
+++ src/main/java/view/ColorPalettes.java
@@ -8,14 +8,14 @@ public final class ColorPalettes {
     private ColorPalettes() {}
 
     public static final List<Color> ALL = List.of(
-            new Color(0x1F77B4), new Color(0xFF7F0E), new Color(0x2CA02C),
-            new Color(0xD62728), new Color(0x9467BD), new Color(0x8C564B),
-            new Color(0xE377C2), new Color(0x7F7F7F), new Color(0xBCBD22),
-            new Color(0x17BECF), new Color(0x0E4D92), new Color(0xF28E2B),
-            new Color(0x59A14F), new Color(0xE15759), new Color(0xB07AA1),
-            new Color(0x9C755F), new Color(0xBAB0AC), new Color(0x76B7B2),
-            new Color(0xF1CE63), new Color(0x4E79A7), new Color(0xA0CBE8),
-            new Color(0xFF9DA7), new Color(0x86BCB6), new Color(0xFABFD2)
+            new Color(0x28365A), new Color(0xE39C61), new Color(0x358F35),
+            new Color(0x881112), new Color(0x600BA1), new Color(0x653D34),
+            new Color(0x009792), new Color(0x2B098A), new Color(0xBCBD22),
+            new Color(0x17BECF), new Color(0x003A59), new Color(0xF47E09),
+            new Color(0x9ADC91), new Color(0xE15759), new Color(0x0F512F),
+            new Color(0x612907), new Color(0x5A5656), new Color(0x76B7B2),
+            new Color(0xF1CE63), new Color(0xA34E79A7, true), new Color(0x80C5F3),
+            new Color(0xFFDD81CC, true), new Color(0x6B4F93), new Color(0xE43D70)
     );
 
     public static List<List<Color>> pages(int n) {
